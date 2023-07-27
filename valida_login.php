d2bdeb0f0c3f79c45b6adc955b9981dfa2f55dbc
wevdiaz <63312710+wevdiaz@users.noreply.github.com>
2023-07-27 19:30:44 -0300

    create autentication user and email - message error

--- valida_login.php
+++ valida_login.php
@@ -1,14 +1,36 @@
 <?php 
+  
+  $usuario_autentificado = false;
 
-   echo '<pre>';
-   print_r($_POST);
-   echo '</pre>';
+  $usuarios_app = array(
+    array('email' => 'adm@teste.com.br', 'senha' => 'abcdw'),
+    array('email' => 'yokito@bol.com.br', 'senha' => '112233')
+  );
 
-   echo '<br/>';
+  
+   foreach($usuarios_app as $user) {
+    
+    if ($_POST['email'] === $user['email'] && $_POST['senha'] === $user['senha']) {
+      $usuario_autentificado = true;
+    }
+    
+   }
+
+   if ($usuario_autentificado) {
+    echo 'Usuário Autenticado com sucesso!';
+   } else {
+    header('Location: index.php?login=erro');
+   }
+
+  //  echo '<pre>';
+  //  print_r($_POST);
+  //  echo '</pre>';
+
+  //  echo '<br/>';
    
-   echo $_POST['email'];
-   echo '<br/>';
-   echo $_POST['senha'];
+  //  echo $_POST['email'];
+  //  echo '<br/>';
+  //  echo $_POST['senha'];
 
    
 ?>
