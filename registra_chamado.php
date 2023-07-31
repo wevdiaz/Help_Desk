5528c4f132fb9d0b5221240db8ee3b92ac9b58b9
wevdiaz <63312710+wevdiaz@users.noreply.github.com>
2023-07-31 20:14:52 -0300

    create view according to profile

--- registra_chamado.php
+++ registra_chamado.php
@@ -1,14 +1,12 @@
 <?php 
 
-  echo '<pre>';
-    print_r($_POST);
-  echo '</pre>';
+  session_start();
 
   $titulo = str_replace('#', '-', $_POST['titulo']);
   $categoria =  str_replace('#', '-', $_POST['categoria']);
   $descricao =  str_replace('#', '-', $_POST['descricao']);
 
-  $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+  $texto = $_SESSION['id'] . '#' . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
 
   $arquivo = fopen('arquivo.hd', 'a');  
 
--- valida_login.php
+++ valida_login.php
@@ -3,10 +3,16 @@
   session_start();
   
   $usuario_autentificado = false;
+  $usuarios_id = null;
+  $usuarios_perfil_id = null;
+
+  $perfis = array(1 => 'Administrativo', 2 => 'Usuario' );
 
   $usuarios_app = array(
-    array('email' => 'adm@teste.com.br', 'senha' => 'abcdw'),
-    array('email' => 'yokito@bol.com.br', 'senha' => '112233')
+    array('id' => 1,'email' => 'adm@teste.com.br', 'senha' => '1234', 'perfil_id' => 1),
+    array('id' => 2,'email' => 'yokito@bol.com.br', 'senha' => '1234', 'perfil_id' => 1),
+    array('id' => 3,'email' => 'bruce@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
+    array('id' => 4,'email' => 'joshua@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
   );
 
   
@@ -14,6 +20,8 @@
     
     if ($_POST['email'] === $user['email'] && $_POST['senha'] === $user['senha']) {
       $usuario_autentificado = true;
+      $usuarios_id = $user['id'];
+      $usuarios_perfil_id = $user['perfil_id'];
     }
     
    }
@@ -21,6 +29,8 @@
    if ($usuario_autentificado) {
     header('Location: home.php');
     $_SESSION['autenticado'] = 'SIM';
+    $_SESSION['id'] = $usuarios_id;
+    $_SESSION['perfil_id'] = $usuarios_perfil_id;
 
    } else {
     header('Location: index.php?login=erro');
